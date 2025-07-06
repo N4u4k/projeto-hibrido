571a7bd4384acb34546b232cce4171bbb4510236
N4u4k <kauan.tga18@gmail.com>
2025-07-05 23:24:17 -0400

    Atualizacao-detalhes

--- js/routes.js
+++ js/routes.js
@@ -31,7 +31,7 @@ var app = new Framework7({
 		},
 		pageInit: function (event, page) {
 		// fazer algo quando a página for inicializada
-    //app.views.main.router.navigate('/detalhes/');
+      app.views.main.router.navigate('/detalhes/');
 
     $.getScript("js/index.js");
     
@@ -154,22 +154,22 @@ var app = new Framework7({
       path: '/detalhes/',
       url: 'detalhes.html',
       animate: false,
-	  on: {
-		pageBeforeIn: function (event, page) {
-		// fazer algo antes da página ser exibida
-    $("#menuprincipal").hide("fast");
-		},
-		pageAfterIn: function (event, page) {
-		// fazer algo depois da página ser exibida
-		},
-		pageInit: function (event, page) {
-		// fazer algo quando a página for inicializada
-		},
-		pageBeforeRemove: function (event, page) {
-		// fazer algo antes da página ser removida do DOM
-		},
-	  }
-    },
+      on: {
+      pageBeforeIn: function (event, page) {
+      // fazer algo antes da página ser exibida
+      $("#menuprincipal").hide("fast");
+      },
+      pageAfterIn: function (event, page) {
+      // fazer algo depois da página ser exibida
+      },
+      pageInit: function (event, page) {
+      // fazer algo quando a página for inicializada
+      },
+      pageBeforeRemove: function (event, page) {
+      // fazer algo antes da página ser removida do DOM
+      },
+      }
+      },
   ],
   // ... other parameters
 });
